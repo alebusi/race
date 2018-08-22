0c353e8b1433f51fa61943150257d3db7e8f87fd
alebusi <allebusi@gmail.com>
2018-08-22 11:23:06 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,6 +1,6 @@
 function myfunc(div) {
   var className = div.getAttribute("class");
-  const legnoC = document.getElementById( 'legno' );
+  var legnoC = document.getElementById( 'legno' );
   legnoC += 1;
   legno.innerHTML = legnoC;
   }
