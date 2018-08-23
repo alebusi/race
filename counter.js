975c68b4cdf556d8a8ca4f3e47dde88ab6e1314b
alebusi <allebusi@gmail.com>
2018-08-23 19:14:09 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,9 +1,13 @@
 var tess = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var success = new Audio('success.wav');
+var decrementa = new Audio('decrementa.mp3');
 
 function myfunc(id,i,ind) {
   tess[ind]+=i;
   v=tess[ind];
   document.getElementById(id).innerHTML = v;
-  success.play();
+  if (i = 1) 
+    success.play();
+  else
+    decrementa.play();
   }
