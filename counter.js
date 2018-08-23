94d2d86aabbea2fb6178895aa573c5994aaf914d
alebusi <allebusi@gmail.com>
2018-08-23 19:32:46 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -6,12 +6,10 @@ function myfunc(id,i,ind) {
   tess[ind]+=i;
   v=tess[ind];
   document.getElementById(id).innerHTML = v;
-  /*
   if (i = 1) {
     success.play();
     }
   else {
     decrementa.play();
-    {
-      */
+    }
   }
