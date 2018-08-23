7a8f0d26dfcbee6e77dd80e472ab06fb2bca65da
alebusi <allebusi@gmail.com>
2018-08-23 18:09:17 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,7 +1,9 @@
 var tess = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var success = new Audio('success.wav');
 
 function myfunc(id,i,ind) {
   tess[ind]+=i;
   v=tess[ind];
   document.getElementById(id).innerHTML = v;
+  success.play();
   }
