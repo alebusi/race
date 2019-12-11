b16ddccfe55aaff0089f0dc427773173aa35879b
alebusi <allebusi@gmail.com>
2019-12-11 11:41:14 +0100

    Update paint.js

--- paint.js
+++ paint.js
@@ -1,7 +1,11 @@
-var colori = ["black","white","orange","#ff3300","#00FFFF","#FF00FF","#00C200"];
+var colori = ["white","orange","#ff3300","#00FFFF","#FF00FF","#00C200","black"];
 var ind_c = 0;
 
 function cambioColore(div) {
+  div.style.backgroundColor = colori[ind_c];
+}
+
+function cambioCampione(div) {
   ind_c+=1;
   if (ind_c > colori.length-1) ind_c=0;
      div.style.backgroundColor = colori[ind_c];
