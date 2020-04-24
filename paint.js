cee7db2c3f98fe6b3bf5b31af50fc58f4b0ba693
alebusi <allebusi@gmail.com>
2020-04-24 14:59:22 +0200

    Update paint.js

--- paint.js
+++ paint.js
@@ -1,12 +1,18 @@
 var colori = ["white","orange","#ff3300","#00FFFF","#FF00FF","#00C200","black"];
 var ind_c = 0;
 
+function verTastoPremuto(e) {
+  if (e.keyCode == 32) {
+      cambioCampione();
+  }	
+}
+
 function cambioColore(div) {
   div.style.backgroundColor = colori[ind_c];
 }
 
-function cambioCampione(div) {
+function cambioCampione() {
   ind_c+=1;
   if (ind_c > colori.length-1) ind_c=0;
-     div.style.backgroundColor = colori[ind_c];
+     document.getElementById("idCampione").style.backgroundColor = colori[ind_c];
 }
