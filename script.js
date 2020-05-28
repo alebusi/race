93f07f2b5ecbe82e8a67eb103e1ae921054f8cdb
alebusi <allebusi@gmail.com>
2020-05-28 15:28:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ mod = 0;
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
 car = new Image();
-car.src = "ferrari2.png";
+car.src = "car_red.png";
 
 window.addEventListener("keydown", keypress_handler, false);
 window.addEventListener("keyup", keyup_handler, false);
